b6147995215bf4872016331c879feee4bde8f488
EmilyHott4 <112112058+EmilyHott4@users.noreply.github.com>
2023-09-17 21:39:25 -0500

    edit4

---
+++ 4_dictionary exercise start file.py
@@ -0,0 +1,104 @@
+# This program uses a dictionary as a deck of cards.
+import random
+def main():
+    # Create a deck of cards.
+    #name of the function we are calling is called create deck
+    #it doesn't have any arguments so you don't need to supply any arguments
+    deck = create_deck() #will call the create deck function and what will be returened is a dictionary
+    #this becomes a dictionary
+    # Get the number of cards to deal.
+    num_cards = int(input('How many cards should I deal? '))
+    #an input(int) is a function that allows you to reciee input from the user
+    #it is a value returning function so you have to save it to some kind of variable
+
+
+
+    # Deal the cards.
+    deal_cards(deck,num_cards)#deck is just an object not a specific thing
+    #with functions you have to recieve arguments then return something
+
+    #we are'nt saving the output to a variable cause we aren't returning anythign
+
+    #if you are returning something, you have to save it into some variable
+
+
+
+    
+    
+
+# The create_deck function returns a dictionary
+# representing a deck of cards.
+def create_deck():
+    # Create a dictionary with each card and its value
+    # stored as key-value pairs.
+    deck = {'Ace of Spades':1, '2 of Spades':2, '3 of Spades':3,
+            '4 of Spades':4, '5 of Spades':5, '6 of Spades':6,
+            '7 of Spades':7, '8 of Spades':8, '9 of Spades':9,
+            '10 of Spades':10, 'Jack of Spades':10,
+            'Queen of Spades':10, 'King of Spades': 10,
+            
+            'Ace of Hearts':1, '2 of Hearts':2, '3 of Hearts':3,
+            '4 of Hearts':4, '5 of Hearts':5, '6 of Hearts':6,
+            '7 of Hearts':7, '8 of Hearts':8, '9 of Hearts':9,
+            '10 of Hearts':10, 'Jack of Hearts':10,
+            'Queen of Hearts':10, 'King of Hearts': 10,
+            
+            'Ace of Clubs':1, '2 of Clubs':2, '3 of Clubs':3,
+            '4 of Clubs':4, '5 of Clubs':5, '6 of Clubs':6,
+            '7 of Clubs':7, '8 of Clubs':8, '9 of Clubs':9,
+            '10 of Clubs':10, 'Jack of Clubs':10,
+            'Queen of Clubs':10, 'King of Clubs': 10,
+            
+            'Ace of Diamonds':1, '2 of Diamonds':2, '3 of Diamonds':3,
+            '4 of Diamonds':4, '5 of Diamonds':5, '6 of Diamonds':6,
+            '7 of Diamonds':7, '8 of Diamonds':8, '9 of Diamonds':9,
+            '10 of Diamonds':10, 'Jack of Diamonds':10,
+            'Queen of Diamonds':10, 'King of Diamonds': 10}
+
+    # Return the deck.
+    return deck
+
+
+
+
+# The deal_cards function deals a specified number of cards
+# from the deck.
+
+def deal_cards(deck, number):
+    # Initialize an accumulator for the hand value.
+    handValue = 0
+
+    
+    
+
+    # Make sure the number of cards to deal is not
+    # greater than the number of cards in the deck.
+    if number > 52:
+        number = 52
+        #data validation to prevent data input being over the number of cards
+
+    
+    
+
+    # Deal the cards and accumulate their values.
+    #popitem method doesn't work
+    for count in range(number): #number is the number of cards the user enteres
+        card = random.choice(list(deck)) #make a list of the keys of the dictionary
+        value = deck[card]
+        handValue += value
+        print(card)
+
+
+    
+
+    # Display the value of the hand.
+    print ('The Value of the hand is',handValue)
+    print()
+    
+    
+
+# Call the main function.
+main()
+#clone repository
+#create virtual environment
+#activate and install
